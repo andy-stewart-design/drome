af94dd029428fc22364c4e30169bea317edaa28c
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2025-12-07 09:05:22 -0500

    Codemirror (#2)
    * initial integration
    
    * adding original theme, flash on eval
    
    * theme cleanup
    
    * more cleanup
    
    * cleanup
    
    * Switching to css vars for cm theme
    
    * search panel styles
    
    * done noodling on search panel
    
    * gutter styling
    
    * theming
    
    * finalizing theme
    
    * updating flash foreground styles

---
+++ apps/repl-web/src/codemirror/theme-reference.ts
@@ -0,0 +1,378 @@
+import { EditorView } from '@codemirror/view'
+import { HighlightStyle, syntaxHighlighting } from '@codemirror/language'
+import { tags as t } from '@lezer/highlight'
+
+const generalTooltip = {
+  borderRadius: '4px',
+  borderRadiusSelected: '3px',
+  lineHeight: '1.3',
+  padding: '4px 8px',
+  paddingRight: '8px',
+}
+
+// UI elements
+const tooltipBackground = '#05101d' // Darker tooltip for better contrast
+
+const abyssTheme = EditorView.theme(
+  {
+    // Base editor styles
+    '&': {
+      color: 'var(--cm-editor-color-fg)',
+      backgroundColor: 'var(--cm-editor-color-bg)',
+      fontSize: 'var(--cm-editor-font-size)',
+      fontFamily: 'var(--cm-editor-font-family)',
+      width: 'var(--cm-editor-width)',
+      height: 'var(--cm-editor-height)',
+      border: 'none',
+      outline: 'none',
+      resize: 'none',
+    },
+
+    // Content and cursor
+    '.cm-content': {
+      caretColor: 'var(--cm-cursor-fg)',
+      lineHeight: 'var(--cm-editor-line-height)',
+    },
+    '.cm-cursor, .cm-dropCursor': {
+      borderLeftColor: 'var(--cm-cursor-fg)',
+      borderLeftWidth: 'var(--cm-cursor-width)',
+    },
+
+    // Selection
+    '&.cm-focused > .cm-scroller > .cm-selectionLayer .cm-selectionBackground, .cm-selectionBackground, .cm-content ::selection':
+      {
+        backgroundColor: 'var(--cm-selection-color-bg)',
+        color: 'var(--cm-selection-color-fg)',
+        borderRadius: 'var(--cm-selection-border-radius)',
+      },
+
+    // Selection matches
+    '.cm-selectionMatch': {
+      backgroundColor: 'var(--cm-selection-match-color-bg)',
+      outline: `var(--cm-selection-match-outline)`,
+      borderRadius: 'var(--cm-selection-match-border-radius)',
+    },
+
+    // Search functionality
+    '.cm-searchMatch': {
+      backgroundColor: 'var(--cm-search-match-color-bg)',
+      color: 'var(--cm-search-match-color-fg)',
+      outline: 'var(--cm-search-match-outline)',
+      borderRadius: 'var(--cm-search-match-border-radius)',
+
+      '& span': {
+        color: 'var(--cm-search-match-color-fg)',
+      },
+    },
+    '.cm-searchMatch.cm-searchMatch-selected': {
+      backgroundColor: 'var(--cm-search-match-selected-color-bg)',
+      color: 'var(--cm-search-match-selected-color-fg)',
+      padding: 'var(--cm-search-match-selected-color-padding)',
+
+      '& span': {
+        color: 'var(--cm-search-match-selected-color-fg)',
+      },
+    },
+
+    // Panels
+    '.cm-panels': {
+      backgroundColor: 'var(--cm-panel-color-bg)',
+      color: 'var(--cm-panel-color-fg)',
+    },
+    '.cm-panels.cm-panels-top': {
+      borderBottom: 'var(--cm-top-panel-border-bottom)',
+    },
+    '.cm-panels.cm-panels-bottom': {
+      borderTop: 'var(--cm-bottom-panel-border-top)',
+    },
+    '.cm-panel button': {
+      backgroundColor: 'var(--cm-panel-button-color-bg)',
+      backgroundImage: 'none',
+      color: 'var(--cm-panel-button-color-fg)',
+      border: 'var(--cm-panel-button-border)',
+      borderRadius: 'var(--cm-panel-button-border-radius)',
+      padding: 'var(--cm-panel-button-padding)',
+    },
+    '.cm-panel button:hover': {
+      backgroundColor: 'var(--cm-panel-button-hover-color-bg)',
+      color: 'var(--cm-panel-button-hover-color-fg)',
+    },
+    '.cm-panel .cm-textfield': {
+      backgroundColor: 'var(--cm-panel-input-color-bg)',
+      color: 'var(--cm-panel-input-color-fg)',
+      border: 'var(--cm-panel-input-border)',
+      borderRadius: 'var(--cm-panel-input-border-radius)',
+      padding: 'var(--cm-panel-input-padding)',
+    },
+    '.cm-panel label input': {
+      backgroundColor: 'var(--cm-panel-input-color-bg)',
+      color: 'var(--cm-panel-input-color-fg)',
+      border: 'var(--cm-panel-input-border)',
+      borderRadius: 'var(--cm-panel-input-border-radius)',
+      padding: 'var(--cm-panel-input-padding)',
+    },
+
+    // Line highlighting
+    '.cm-activeLine': {
+      backgroundColor: 'var(--cm-active-line-color-bg)',
+      borderRadius: 'var(--cm-active-line-border-radius)',
+      outline: 'var(--cm-active-line-outline)',
+    },
+
+    // Gutters
+    '.cm-gutters': {
+      backgroundColor: 'var(--cm-gutters-color-bg)',
+      color: 'var(--cm-gutters-color-fg)',
+    },
+    '.cm-gutters .cm-gutterElement': {
+      display: 'flex',
+      justifyContent: 'flex-end',
+      alignItems: 'center',
+    },
+    '.cm-activeLineGutter': {
+      backgroundColor: 'var(--cm-gutters-active-line-color-bg)',
+      color: 'var(--cm-gutters-active-line-color-fg)',
+      outline: 'var(--cm-active-line-outline)',
+    },
+    '.cm-lineNumbers': {
+      fontSize: 'var(--cm-gutters-font-size)',
+    },
+    '.cm-foldGutter': {
+      fontSize: 'var(--cm-gutters-font-size)',
+    },
+
+    // Tooltips and autocomplete
+    '.cm-tooltip': {
+      backgroundColor: tooltipBackground,
+      border: '1px solid #084671',
+      borderRadius: generalTooltip.borderRadius,
+      padding: generalTooltip.padding,
+      boxShadow: '0 2px 8px rgba(0, 0, 0, 0.5)',
+    },
+    '.cm-tooltip-autocomplete': {
+      '& > ul > li': {
+        padding: generalTooltip.padding,
+        lineHeight: generalTooltip.lineHeight,
+      },
+      '& > ul > li[aria-selected]': {
+        backgroundColor: '#084671',
+        color: '#e0edff',
+        borderRadius: generalTooltip.borderRadiusSelected,
+      },
+      '& > ul > li > span.cm-completionIcon': {
+        color: '#5f7e97',
+        paddingRight: generalTooltip.paddingRight,
+      },
+      '& > ul > li > span.cm-completionDetail': {
+        color: '#5f7e97',
+        fontStyle: 'italic',
+      },
+    },
+    '.cm-tooltip .cm-tooltip-arrow:before': {
+      borderTopColor: 'transparent',
+      borderBottomColor: 'transparent',
+    },
+    '.cm-tooltip .cm-tooltip-arrow:after': {
+      borderTopColor: tooltipBackground,
+      borderBottomColor: tooltipBackground,
+    },
+
+    // Diagnostics styling
+    // '.cm-diagnostic': {
+    //   '&-error': {
+    //     borderLeft: `3px solid ${invalid}`,
+    //   },
+    //   '&-warning': {
+    //     borderLeft: `3px solid ${colorNeonPink}`,
+    //   },
+    //   '&-info': {
+    //     borderLeft: `3px solid ${colorPurple}`,
+    //   },
+    // },
+    // '.cm-lintPoint-error': {
+    //   borderBottom: `2px wavy ${invalid}`,
+    // },
+    // '.cm-lintPoint-warning': {
+    //   borderBottom: `2px wavy ${colorNeonPink}`,
+    // },
+
+    // Matching brackets
+    // '.cm-matchingBracket': {
+    //   backgroundColor: activeBracketBg,
+    //   outline: `1px solid ${activeBracketBorder}`,
+    //   borderRadius: generalMatching.borderRadius,
+    // },
+    // '.cm-nonmatchingBracket': {
+    //   backgroundColor: '#780e1e80',
+    //   outline: `1px solid ${invalid}`,
+    //   borderRadius: generalMatching.borderRadius,
+    // },
+
+    // Fold placeholder
+    // '.cm-foldPlaceholder': {
+    //   backgroundColor: 'transparent',
+    //   color: '#5f7e97',
+    //   fontStyle: 'italic',
+    //   border: `1px dotted ${activeBracketBorder}`,
+    //   borderRadius: generalPlaceholder.borderRadius,
+    //   padding: generalPlaceholder.padding,
+    //   margin: generalPlaceholder.margin,
+    // },
+
+    // Focus outline
+    '&.cm-focused': {
+      outline: 'none',
+    },
+
+    // Scrollbars
+    '& .cm-scroller::-webkit-scrollbar': {
+      width: 'var(--cm-scrollbar-width)',
+      height: 'var(--cm-scrollbar-height)',
+    },
+    '& .cm-scroller::-webkit-scrollbar-track': {
+      background: 'var(--cm-scrollbar-color-bg)',
+    },
+    '& .cm-scroller::-webkit-scrollbar-thumb': {
+      backgroundColor: 'var(--cm-scrollbar-thumb-color-bg)',
+      border: 'var(--cm-scrollbar-thumb-border)',
+      borderRadius: 'var(--cm-scrollbar-thumb-border-radius)',
+    },
+    '& .cm-scroller::-webkit-scrollbar-thumb:hover': {
+      backgroundColor: 'var(--cm-scrollbar-thumb-hover-color-bg)',
+    },
+    '& .cm-scroller::-webkit-scrollbar-corner': {
+      backgroundColor: 'var(--cm-scrollbar-corner-color-bg)',
+      border: 'var(--cm-scrollbar-corner-border)',
+    },
+  },
+  { dark: true },
+)
+
+/**
+ * Enhanced syntax highlighting for the Abyss theme
+ */
+const highlightStyle = HighlightStyle.define([
+  // Keywords and control flow
+  {
+    tag: t.keyword,
+    color: 'var(--cm-declaration-keyword-color-fg)',
+    fontWeight: 'regular',
+  },
+  {
+    tag: t.moduleKeyword,
+    color: 'var(--cm-module-keyword-color-fg)',
+    fontWeight: 'regular',
+  },
+
+  // Names and variables
+  {
+    tag: [t.definition(t.variableName)],
+    color: 'var(--cm-variable-declaration-color-fg)',
+  },
+  { tag: [t.variableName], color: 'var(--cm-variable-reference-color-fg)' },
+  {
+    tag: [t.propertyName],
+    color: 'var(--cm-property-name-color-fg)',
+    fontStyle: 'normal',
+  },
+  {
+    tag: [t.definition(t.propertyName)],
+    color: 'var(--cm-property-object-key-color-fg)',
+    fontStyle: 'normal',
+  },
+  {
+    tag: [t.function(t.definition(t.variableName))],
+    color: 'var(--cm-function-declaration-color-fg)',
+  },
+  {
+    tag: [t.function(t.variableName), t.labelName],
+    color: 'var(--cm-function-reference-color-fg)',
+  },
+
+  // Classes and types
+  {
+    tag: [t.className],
+    color: 'var(--cm-class-identifier-color-fg)',
+    fontStyle: 'italic',
+  },
+  //   { tag: [t.namespace], color: colorNeonPink, fontStyle: 'italic' },
+
+  // Operators and punctuation - clearer blues
+  //   { tag: [t.operator, t.operatorKeyword], color: 'red' },
+  //   { tag: [t.bracket], color: 'red' },
+  //   { tag: [t.brace], color: 'red' },
+  //   { tag: [t.punctuation], color: 'red' },
+
+  // Constants and literals
+  { tag: t.number, color: 'var(--cm-number-color-fg)' },
+  { tag: t.bool, color: 'var(--cm-boolean-color-fg)' },
+  //   { tag: t.changed, color: 'red' },
+  //   { tag: t.annotation, color: 'red', fontStyle: 'italic' },
+  //   { tag: t.modifier, color: 'red', fontStyle: 'italic' },
+  { tag: t.self, color: 'var(--cm-self-color-fg)' },
+  //   {
+  //     tag: [t.color, t.constant(t.name), t.standard(t.name)],
+  //     color: 'green',
+  //   },
+  //   { tag: [t.atom, t.special(t.variableName)], color: 'green' },
+
+  // Strings and regex
+  //   { tag: [t.processingInstruction, t.inserted], color: 'green' },
+  { tag: t.regexp, color: 'var(--cm-regex-color-fg)' },
+
+  // Punctuation and structure
+  //   { tag: t.definition(t.typeName), color: colorPurple, fontWeight: 'bold' },
+  {
+    tag: [t.operator, t.operatorKeyword],
+    color: 'var(--cm-operator-color-fg)',
+  },
+  { tag: [t.bracket], color: 'var(--cm-bracket-color-fg)' },
+  { tag: [t.brace], color: 'var(--cm-bracket-color-fg)' },
+  { tag: [t.punctuation], color: 'var(--cm-bracket-color-fg)' },
+
+  // Comments and documentation
+  { tag: t.meta, color: 'var(--cm-comment-color-fg)' },
+  { tag: t.comment, fontStyle: 'italic', color: 'var(--cm-comment-color-fg)' },
+  {
+    tag: t.docComment,
+    fontStyle: 'italic',
+    color: 'var(--cm-comment-color-fg)',
+  },
+
+  // HTML/XML elements
+  //   { tag: [t.tagName], color: base0F },
+  //   { tag: [t.attributeName], color: '#ffd580' },
+
+  // Markdown and text formatting
+  //   { tag: [t.heading], fontWeight: 'bold', color: colorNeutral[50] },
+  //   { tag: [t.strong], fontWeight: 'bold' },
+  //   { tag: [t.emphasis], fontStyle: 'italic' },
+
+  // Links and URLs
+  //   { tag: [t.link], color: base10, fontWeight: '500' },
+  //   {
+  //     tag: [t.url],
+  //     color: base11,
+  //     textDecoration: 'underline',
+  //     textUnderlineOffset: '2px',
+  //   },
+
+  // Special states
+  //   { tag: [t.invalid], color: invalid, textDecoration: 'underline wavy' },
+  //   { tag: [t.strikethrough], color: invalid, textDecoration: 'line-through' },
+
+  // Enhanced syntax highlighting
+  //   { tag: t.constant(t.name), color: colorNeonPink },
+  {
+    tag: t.controlKeyword,
+    color: 'var(--cm-control-keyword-color-fg)',
+    fontWeight: 'bold',
+  },
+  // { tag: t.deleted, color: base08 },
+  //   { tag: t.labelName, color: 'green' },
+  { tag: t.string, color: 'var(--cm-string-color-fg)' },
+])
+
+const theme = [abyssTheme, syntaxHighlighting(highlightStyle)]
+
+export { theme }
--- apps/repl-web/src/routes/index.tsx
+++ apps/repl-web/src/routes/index.tsx
@@ -1,21 +1,53 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { useEffect, useState, type KeyboardEvent } from 'react'
 import Drome from 'drome-live'
+import { createFileRoute } from '@tanstack/react-router'
+import { useEffect, useRef, useState } from 'react'
+import { basicSetup, EditorView } from 'codemirror'
+import { theme } from '@/codemirror/theme'
+import { javascript } from '@/codemirror/language'
+import { flash, flashField } from '@/codemirror/flash'
+import '@/codemirror/theme-default.css'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
+  const editorContainer = useRef<HTMLDivElement>(null)
   const [drome, setDrome] = useState<Drome | null>(null)
+  const [editor, setEditor] = useState<EditorView | null>(null)
 
   useEffect(() => {
+    if (!editorContainer.current) return
     Drome.init(120).then((d) => setDrome(d))
+
+    const editor = new EditorView({
+      doc: 'd.sample("bd:3").bank("tr909").euclid([3, 5], 8)',
+      extensions: [basicSetup, theme, javascript(), flashField],
+      parent: editorContainer.current,
+    })
+
+    setEditor(editor)
+
+    return () => editor.destroy()
   }, [])
 
+  useEffect(() => {
+    if (!drome || !editor) return
+
+    window.addEventListener('keydown', onkeyDown)
+
+    return () => window.removeEventListener('keydown', onkeyDown)
+  }, [drome, editor])
+
   function onkeyDown(e: KeyboardEvent) {
-    if (!(e.target instanceof HTMLTextAreaElement) || !drome) return
+    console.log('keydown', e.key)
+    console.log()
+
+    if (!drome || !editor) return
+
     if (e.altKey && e.key === 'Enter') {
       e.preventDefault()
-      runCode(drome, e.target.value)
+
+      runCode(drome, editor.state.doc.toString())
+      flash(editor)
       if (drome.paused) {
         drome.start()
       }
@@ -28,13 +60,13 @@ function App() {
 
   return (
     <main>
-      <textarea
+      <div ref={editorContainer} />
+      {/* <textarea
         name="code"
         defaultValue='d.sample("bd:3").bank("tr909").euclid([3, 5], 8)'
-        onKeyDown={onkeyDown}
         spellCheck={false}
         disabled={!drome}
-      />
+      /> */}
     </main>
   )
 }
