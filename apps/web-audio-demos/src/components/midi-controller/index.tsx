8de048e6bfc6412e188b281375cc2d7f1ea40adb
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-01-11 14:34:12 -0500

    exploring web midi api

--- apps/web-audio-demos/src/components/midi-controller/index.tsx
+++ apps/web-audio-demos/src/components/midi-controller/index.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 
 export default function MidiController() {
   const [midi, setMidi] = useState<MIDIAccess | null>(null);
+  const [inputs, setInputs] = useState<MIDIInput[]>([]);
 
   useEffect(() => {
     const ctrl = new AbortController();
@@ -10,6 +11,17 @@ export default function MidiController() {
       try {
         const midi = await navigator.requestMIDIAccess();
         setMidi(midi);
+        setInputs(Array.from(midi.inputs.values()));
+
+        midi.addEventListener(
+          "statechange",
+          (e) => {
+            if (e.target instanceof MIDIAccess) {
+              setInputs(Array.from(e.target.inputs.values()));
+            }
+          },
+          ctrl,
+        );
 
         midi.inputs.forEach((input) => {
           input.addEventListener(
@@ -28,22 +40,73 @@ export default function MidiController() {
     return ctrl.abort;
   }, []);
 
-  return midi ? (
+  return midi && inputs ? (
     <div>
       <h2>Midi Inputs</h2>
       <ul>
-        {Array.from(midi.inputs).map(([_, input]) => (
-          <li key={input.id}>{input.name}</li>
+        {inputs.map((input) => (
+          <li key={input.id}>
+            {input.name} ({input.id})
+          </li>
         ))}
       </ul>
       <h2>Midi Outputs</h2>
+      <div>
+        {/*<button
+          onClick={() => {
+            const out = getMidiInputByName(midi, "OP-1");
+            out?.send([0x91, 60, 127]);
+            out?.send([0x81, 60, 127], window.performance.now() + 1000.0);
+          }}
+          // onMouseDown={() => {
+          //   const out = getMidiInputByName(midi, "OP-1");
+          //   out?.send([0x91, 60, 127], window.performance.now() + 1000.0);
+          // }}
+          // onMouseUp={() => {
+          //   const out = getMidiInputByName(midi, "OP-1");
+          //   out?.send([0x81, 60, 127], window.performance.now() + 1000.0);
+          // }}
+        >
+          Send MIDI
+        </button>*/}
+      </div>
       <ul>
         {Array.from(midi.outputs).map(([_, output]) => (
-          <li key={output.id}>{output.name}</li>
+          <li key={output.id}>
+            {output.name} ({output.id})
+            <button
+              // onClick={() => {
+              //   const out = getMidiInputByName(midi, "OP-1");
+              //   out?.send([0x91, 60, 127]);
+              //   out?.send([0x81, 60, 127], window.performance.now() + 1000.0);
+              // }}
+              onMouseDown={() => {
+                const out = getMidiInputByName(midi, "OP-1");
+                out?.send([0x91, 60, 127]);
+              }}
+              onMouseUp={() => {
+                const out = getMidiInputByName(midi, "OP-1");
+                out?.send([0x81, 60, 127]);
+              }}
+            >
+              Send MIDI
+            </button>
+          </li>
         ))}
       </ul>
     </div>
   ) : (
     <p>Waiting for midi access...</p>
   );
+}
+
+function getMidiInputByName(midi: MIDIAccess, name: string) {
+  for (const [_, output] of midi.outputs) {
+    if (output.name === name) {
+      console.log(`Found input port: ${output.name}, id: ${output.id}`);
+      return output;
+    }
+  }
+  console.warn(`No input port found with name: ${name}`);
+  return undefined;
 }
