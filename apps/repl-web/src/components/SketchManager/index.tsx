67644f15be2e8a62b13d1bc7dac696a350a95a97
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-02-07 09:43:46 -0500

    sidebar icons

--- apps/repl-web/src/components/SketchManager/index.tsx
+++ apps/repl-web/src/components/SketchManager/index.tsx
@@ -23,10 +23,10 @@ function SketchManager({
     <div>
       <div class={s.toolbar}>
         <button classList={clst(s.button, s.tool)} onClick={onSave}>
-          Save
+          <IconArrowDown12 /> Save
         </button>
         <button classList={clst(s.button, s.tool)} onClick={onCreateNew}>
-          + New
+          <IconPlus12 /> New
         </button>
       </div>
 
@@ -37,7 +37,7 @@ function SketchManager({
               <button
                 classList={clst(s.button, s.primary)}
                 onClick={() => onReplace(sketch)}
-                data-current={currentSketch().title === sketch.title}
+                data-currentz={currentSketch().title === sketch.title}
               >
                 <p>{sketch.title}</p>
                 <p>{sketch.author}</p>
@@ -46,7 +46,7 @@ function SketchManager({
                 classList={clst(s.button, s.delete)}
                 onClick={() => onDelete(sketch.id)}
               >
-                ✕
+                <IconClose12 />
               </button>
             </li>
           )}
@@ -60,4 +60,32 @@ export default SketchManager
 
 function clst(...classNames: string[]) {
   return Object.fromEntries(classNames.map((cn) => [cn, true]))
+}
+
+function IconPlus12() {
+  return (
+    <svg viewBox="0 0 12 12" width={12} height={12}>
+      <path d="M 6 2 L 6 10 M 2 6 L 10 6" stroke="currentColor" fill="none" />
+    </svg>
+  )
+}
+
+function IconArrowDown12() {
+  return (
+    <svg viewBox="0 0 12 12" width={12} height={12}>
+      <path
+        d="M 6 1 L 6 10 M 2 6 L 6 10 L 10 6"
+        stroke="currentColor"
+        fill="none"
+      />
+    </svg>
+  )
+}
+
+function IconClose12() {
+  return (
+    <svg viewBox="0 0 12 12" width={12} height={12}>
+      <path d="M 2 2 L 10 10 M 2 10 L 10 2" stroke="currentColor" fill="none" />
+    </svg>
+  )
 }
