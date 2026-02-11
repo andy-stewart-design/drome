018f36f0e6ae109d93ec82520534871bb2eb2374
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-02-11 14:00:15 -0500

    layout component

--- apps/repl-web/src/routes/index.tsx
+++ apps/repl-web/src/routes/index.tsx
@@ -6,48 +6,31 @@ import EditorToolbar from '@/components/EditorToolbar'
 import SketchMetadata from '@/components/SketchMetadata'
 import SketchManager from '@/components/SketchManager'
 import SidebarResizer from '@/components/SidebarResizer'
-import { useSidebar } from '@/providers/sidebar'
 import { useKeyboardEvent } from '@/utils/use-keyboard-event'
+import MainLayout from '@/components/main-layout'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  const { showSidebar, sidebarSize } = useSidebar()
   useKeyboardEvent()
 
   return (
-    <div
-      style={{
-        display: 'grid',
-        'grid-template-columns': `minmax(0,1fr) ${showSidebar() ? sidebarSize() : 0}px`,
-      }}
-    >
-      <div
-        style={{
-          width: '100%',
-          height: '100dvh',
-          display: 'flex',
-          'flex-direction': 'column',
-        }}
-      >
-        <EditorHeader>
-          <SketchMetadata />
-          <EditorToolbar />
-        </EditorHeader>
-        <CodeMirror />
-      </div>
-      <div
-        style={{
-          display: showSidebar() ? 'block' : 'none',
-          position: 'relative',
-          overflow: 'auto',
-          'overscroll-behavior': 'contain',
-          'border-inline-start': '1px solid rgb(255 255 255 / 0.15);',
-        }}
-      >
-        <SketchManager />
-        <SidebarResizer />
-      </div>
-    </div>
+    <MainLayout
+      content={
+        <>
+          <EditorHeader>
+            <SketchMetadata />
+            <EditorToolbar />
+          </EditorHeader>
+          <CodeMirror />
+        </>
+      }
+      sidebar={
+        <>
+          <SketchManager />
+          <SidebarResizer />
+        </>
+      }
+    />
   )
 }
