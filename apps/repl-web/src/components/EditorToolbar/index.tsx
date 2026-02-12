ec18eb584f1c03521eb322cce8c3adbb9e271492
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-02-12 06:16:49 -0500

    refresh icon

--- apps/repl-web/src/components/EditorToolbar/index.tsx
+++ apps/repl-web/src/components/EditorToolbar/index.tsx
@@ -1,7 +1,7 @@
 import { Show } from 'solid-js'
-import IconPause20 from '@/components/icons/IconPause20'
-import IconPlay20 from '@/components/icons/IconPlay20'
-import IconSidebar20 from '@/components/icons/IconSidebar20'
+import IconPause20 from '@/components/icons/icon-pause-20'
+import IconPlay20 from '@/components/icons/icon-play-20'
+import IconSidebar20 from '@/components/icons/icon-sidebar-20'
 import { usePlayState } from '@/providers/playstate'
 import { useSidebar } from '@/providers/sidebar'
 import { useSession } from '@/providers/session'
@@ -9,6 +9,7 @@ import { useDrome } from '@/providers/drome'
 import { useEditor } from '@/providers/editor'
 import { flash } from '@/codemirror/flash'
 import s from './style.module.css'
+import IconRefresh20 from '../icons/icon-refresh-20'
 
 function EditorToolbar() {
   const { drome } = useDrome()
@@ -40,16 +41,10 @@ function EditorToolbar() {
       <Show when={!paused()}>
         <span class={s.beat}>{beat()}</span>
         <button
-          aria-label={`Play/pause music`}
+          aria-label={`Evaluate code`}
           onClick={() => togglePlaystate(true)}
         >
-          <svg viewBox="0 0 20 20" fill="currentColor" width={20} height={20}>
-            <path
-              fill-rule="evenodd"
-              clip-rule="evenodd"
-              d="M15.312 11.424a5.5 5.5 0 0 1-9.201 2.466l-.312-.311h2.433a.75.75 0 0 0 0-1.5H3.989a.75.75 0 0 0-.75.75v4.242a.75.75 0 0 0 1.5 0v-2.43l.31.31a7 7 0 0 0 11.712-3.138.75.75 0 0 0-1.449-.39Zm1.23-3.723a.75.75 0 0 0 .219-.53V2.929a.75.75 0 0 0-1.5 0V5.36l-.31-.31A7 7 0 0 0 3.239 8.188a.75.75 0 1 0 1.448.389A5.5 5.5 0 0 1 13.89 6.11l.311.31h-2.432a.75.75 0 0 0 0 1.5h4.243a.75.75 0 0 0 .53-.219Z"
-            />
-          </svg>
+          <IconRefresh20 />
         </button>
       </Show>
       <button aria-label={`Play/pause music`} onClick={() => togglePlaystate()}>
