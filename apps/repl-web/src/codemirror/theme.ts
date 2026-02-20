c23c9749abb6c812ad796fcc65d3972ec58e9bf4
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-02-20 07:17:49 -0500

    vars

--- apps/repl-web/src/codemirror/theme.ts
+++ apps/repl-web/src/codemirror/theme.ts
@@ -6,8 +6,8 @@ const ALLOW_FOLD = false
 
 const editorTheme = EditorView.theme({
   '&': {
-    backgroundColor: 'var(--color-cm-background)',
-    color: 'var(--color-cm-text)',
+    backgroundColor: 'var(--cm-color-background)',
+    color: 'var(--cm-color-text)',
     fontSize: 'var(--cm-editor-font-size)',
   },
   // Focus outline
@@ -15,62 +15,67 @@ const editorTheme = EditorView.theme({
     outline: 'none',
   },
   '.cm-content': {
-    caretColor: 'var(--color-cm-caret)',
+    caretColor: 'var(--cm-color-caret)',
     lineHeight: 'var(--cm-line-height, 1.6)',
   },
   '.cm-cursor': {
-    borderLeftColor: 'var(--color-cm-caret)',
+    borderLeft: 'var(--cm-caret-width, 1.5px) solid var(--cm-color-caret)',
   },
   '.cm-gutters': {
-    backgroundColor: 'var(--color-cm-gutter-bg)',
-    color: 'var(--color-cm-gutter-fg)',
-    borderRight: '1px solid var(--color-cm-gutter-border)',
+    backgroundColor: 'var(--cm-color-gutter-bg)',
+    color: 'var(--cm-color-gutter-fg)',
+    borderRight: '1px solid var(--cm-color-gutter-border)',
   },
   '.cm-activeLineGutter': {
-    backgroundColor: 'var(--color-cm-active-line-gutter-bg)',
-    color: 'var(--color-cm-active-line-gutter-fg)',
+    backgroundColor: 'var(--cm-color-active-line-gutter-bg)',
+    color: 'var(--cm-color-active-line-gutter-fg)',
   },
   '& .cm-line': {
-    backgroundColor: 'var(--color-cm-line-bg)',
+    backgroundColor: 'var(--cm-color-line-bg)',
     inlineSize: 'var(--cm-line-inline-size)',
   },
   '& .cm-line.cm-activeLine': {
-    backgroundColor: 'var(--color-cm-active-line-bg)',
+    backgroundColor: 'var(--cm-color-active-line-bg)',
+    borderRadius: 'var(--cm-active-line-radius, 3px)',
   },
   '.cm-selectionBackground, &.cm-focused > .cm-scroller > .cm-selectionLayer .cm-selectionBackground':
     {
-      backgroundColor: 'var(--color-cm-selection-bg)',
-      borderRadius: '1px',
-      outline: '4px solid var(--color-cm-selection-bg)',
+      backgroundColor: 'var(--cm-color-selection-bg)',
+      borderRadius: 'var(--cm-selection-radius, 2px)',
+      outline:
+        'var(--cm-selection-outline-width, 2px) solid var(--cm-color-selection-bg)',
     },
   '& .cm-line .cm-matchingBracket, & .cm-line .cm-nonmatchingBracket': {
     outline: 'none',
   },
   '& .cm-line .cm-matchingBracket': {
-    backgroundColor: 'var(--color-cm-matching-bracket-bg)',
+    backgroundColor: 'var(--cm-color-matching-bracket-bg)',
   },
   '& .cm-line .cm-matchingBracket > span': {
-    color: 'var(--color-cm-matching-bracket-fg)',
+    color: 'var(--cm-color-matching-bracket-fg)',
   },
   '& .cm-line .cm-nonmatchingBracket': {
-    backgroundColor: 'var(--color-cm-nonmatching-bracket-bg)',
+    backgroundColor: 'var(--cm-color-nonmatching-bracket-bg)',
   },
   '& .cm-line .cm-nonmatchingBracket > span': {
-    color: 'var(--color-cm-nonmatching-bracket-fg)',
+    color: 'var(--cm-color-nonmatching-bracket-fg)',
   },
   '& .cm-line .cm-selectionMatch': {
-    backgroundColor: 'var(--color-cm-selection-match-bg)',
+    backgroundColor: 'var(--cm-color-selection-match-bg)',
   },
   '& .cm-line .cm-selectionMatch > span': {
-    color: 'var(--color-cm-selection-match-fg)',
+    color: 'var(--cm-color-selection-match-fg)',
   },
   '& .cm-gutters > .cm-gutter.cm-lineNumbers ': {
-    color: 'var(--color-cm-line-numbers, coral)',
+    fontSize: '12px',
+    color: 'var(--cm-color-line-numbers, coral)',
+    paddingInlineStart: '0.5rem',
   },
   '& .cm-gutters > .cm-gutter.cm-lineNumbers > .cm-gutterElement': {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'flex-end',
+    paddingBlockStart: '0.875px',
   },
   '& .cm-gutters > .cm-gutter.cm-foldGutter': {
     pointerEvents: ALLOW_FOLD ? 'auto' : 'none',
@@ -82,47 +87,47 @@ const editorTheme = EditorView.theme({
   '& .cm-scroller': {
     scrollbarWidth: 'thin',
     scrollbarColor:
-      'var(--color-cm-scrollbar-thumb) var(--color-cm-scrollbar-track)',
+      'var(--cm-color-scrollbar-thumb) var(--cm-color-scrollbar-track)',
   },
 })
 
 const highlightStyle = HighlightStyle.define([
-  { tag: tags.keyword, color: 'var(--color-cm-keyword)' },
-  { tag: tags.controlKeyword, color: 'var(--color-cm-control-keyword)' },
-  { tag: tags.operator, color: 'var(--color-cm-operator)' },
-  { tag: tags.punctuation, color: 'var(--color-cm-punctuation)' },
-  { tag: tags.string, color: 'var(--color-cm-string)' },
-  { tag: tags.number, color: 'var(--color-cm-number)' },
-  { tag: tags.bool, color: 'var(--color-cm-bool)' },
-  { tag: tags.null, color: 'var(--color-cm-null)' },
-  { tag: tags.variableName, color: 'var(--color-cm-variable)' },
+  { tag: tags.keyword, color: 'var(--cm-color-keyword)' },
+  { tag: tags.controlKeyword, color: 'var(--cm-color-control-keyword)' },
+  { tag: tags.operator, color: 'var(--cm-color-operator)' },
+  { tag: tags.punctuation, color: 'var(--cm-color-punctuation)' },
+  { tag: tags.string, color: 'var(--cm-color-string)' },
+  { tag: tags.number, color: 'var(--cm-color-number)' },
+  { tag: tags.bool, color: 'var(--cm-color-bool)' },
+  { tag: tags.null, color: 'var(--cm-color-null)' },
+  { tag: tags.variableName, color: 'var(--cm-color-variable)' },
   {
     tag: tags.definition(tags.variableName),
-    color: 'var(--color-cm-definition)',
+    color: 'var(--cm-color-definition)',
   },
   {
     tag: tags.function(tags.variableName),
-    color: 'var(--color-cm-function)',
+    color: 'var(--cm-color-function)',
   },
-  { tag: tags.propertyName, color: 'var(--color-cm-property)' },
+  { tag: tags.propertyName, color: 'var(--cm-color-property)' },
   {
     tag: tags.comment,
-    color: 'var(--color-cm-comment)',
+    color: 'var(--cm-color-comment)',
     fontStyle: 'italic',
   },
   {
     tag: tags.lineComment,
-    color: 'var(--color-cm-comment)',
+    color: 'var(--cm-color-comment)',
     fontStyle: 'italic',
   },
   {
     tag: tags.blockComment,
-    color: 'var(--color-cm-comment)',
+    color: 'var(--cm-color-comment)',
     fontStyle: 'italic',
   },
-  { tag: tags.typeName, color: 'var(--color-cm-type)' },
-  { tag: tags.className, color: 'var(--color-cm-class)' },
-  { tag: tags.regexp, color: 'var(--color-cm-regexp)' },
+  { tag: tags.typeName, color: 'var(--cm-color-type)' },
+  { tag: tags.className, color: 'var(--cm-color-class)' },
+  { tag: tags.regexp, color: 'var(--cm-color-regexp)' },
 ])
 
 const theme = [editorTheme, syntaxHighlighting(highlightStyle)]
