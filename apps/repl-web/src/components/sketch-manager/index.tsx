376c458ec1838ad2b7449e8311ae20b886b24c36
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-02-27 06:14:18 -0500

    initial editable label

--- apps/repl-web/src/components/sketch-manager/index.tsx
+++ apps/repl-web/src/components/sketch-manager/index.tsx
@@ -1,4 +1,4 @@
-import { For } from 'solid-js'
+import { createSignal, For, Show, type Accessor } from 'solid-js'
 import { useSession } from '@/providers/session'
 import { useEditor } from '@/providers/editor'
 import s from './style.module.css'
@@ -42,22 +42,13 @@ function SketchManager() {
       <ul class={s.list}>
         <For each={savedSketches()}>
           {(sketch) => (
-            <li class={s.item}>
-              <button
-                classList={clst(s.button, s.primary)}
-                onClick={() => setWorkingSketch(sketch)}
-                data-current={workingSketch().title === sketch.title}
-              >
-                <p>{sketch.title}</p>
-                <p>{sketch.author}</p>
-              </button>
-              <button
-                classList={clst(s.button, s.delete)}
-                onClick={() => handleDelete(sketch.id)}
-              >
-                <IconClose12 />
-              </button>
-            </li>
+            <SketchLabel
+              title={sketch.title}
+              updated={sketch.updatedAt}
+              selected={() => workingSketch().title === sketch.title}
+              onSelect={() => setWorkingSketch(sketch)}
+              onDelete={() => handleDelete(sketch.id)}
+            />
           )}
         </For>
       </ul>
@@ -66,6 +57,115 @@ function SketchManager() {
 }
 
 export default SketchManager
+
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+// -----------------------------------------------------------------
+
+interface SketchLabelProps {
+  title: string
+  updated: string
+  selected: Accessor<boolean>
+  onSelect(): void
+  onDelete(): void
+}
+
+function SketchLabel({
+  title,
+  updated,
+  selected,
+  onSelect,
+  onDelete,
+}: SketchLabelProps) {
+  const updatedFormatted = new Intl.DateTimeFormat('en-US').format(
+    new Date(updated),
+  )
+
+  return (
+    <li class={s.label}>
+      <button
+        classList={clst(s.button, s.select_button)}
+        onClick={onSelect}
+        data-current={selected()}
+      />
+      <div class={s.label_content}>
+        <div class={s.label_text}>
+          <EditableText text={title} />
+          <p class={s.label_date}>{updatedFormatted}</p>
+        </div>
+        <button classList={clst(s.button, s.delete_button)} onClick={onDelete}>
+          <IconClose12 />
+        </button>
+      </div>
+    </li>
+  )
+}
+
+function EditableText({ text }: { text: string }) {
+  const [editing, setEditing] = createSignal(false)
+  let inputRef: HTMLParagraphElement | undefined
+  let triggerRef: HTMLButtonElement | undefined
+
+  function handleKeyDown(e: KeyboardEvent) {
+    if (!inputRef) return
+
+    if (e.key === 'Enter' || e.key === 'Escape') {
+      handleEditEnd(e)
+      if (e.key === 'Escape') inputRef.innerHTML = text
+    }
+  }
+
+  function handleEditEnd(e: Event) {
+    e.preventDefault()
+    setEditing(false)
+    triggerRef?.focus()
+  }
+
+  return (
+    <div class={s.editable_container}>
+      <p
+        ref={inputRef}
+        class={s.label_title}
+        contentEditable={editing()}
+        spellcheck="false"
+        onKeyDown={handleKeyDown}
+        onBlur={handleEditEnd}
+      >
+        {text}
+      </p>
+      <Show when={!editing()}>
+        <button
+          ref={triggerRef}
+          class={s.button}
+          onClick={() => {
+            setEditing(true)
+            inputRef?.focus()
+          }}
+        >
+          Edit
+        </button>
+      </Show>
+    </div>
+    // <>
+    //   <Show when={!editing()}>
+    //     <p>
+    //       {text} <span onClick={() => setEditing(true)}>Edit</span>
+    //     </p>
+    //   </Show>
+    //   <Show when={editing()}>
+    //     <input
+    //       ref={inputRef}
+    //       value={text}
+    //       onKeyDown={(e) => {
+    //         if (e.key === 'Enter' || e.key === 'Escape') {
+    //           setEditing(false)
+    //         }
+    //       }}
+    //     />
+    //   </Show>
+    // </>
+  )
+}
 
 function clst(...classNames: string[]) {
   return Object.fromEntries(classNames.map((cn) => [cn, true]))
