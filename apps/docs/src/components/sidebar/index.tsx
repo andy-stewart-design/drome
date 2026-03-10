d4746ea66d86c4701ca54f7283e32699603b9ce7
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-03-10 17:43:36 -0400

    basic scrolling sidebar

--- apps/docs/src/components/sidebar/index.tsx
+++ apps/docs/src/components/sidebar/index.tsx
@@ -1,27 +1,27 @@
 import { For } from "solid-js";
 import type { TreeItem } from "@/utils/sidebar";
-import styles from "./style.module.css";
+import s from "./style.module.css";
 import type { CollectionEntry } from "astro:content";
 
-interface SidebarProps<T extends { id: string }> {
+interface SidebarProps<T extends CollectionEntry<"docs">> {
   tree: TreeItem<T>[];
 }
 
 function Sidebar<T extends CollectionEntry<"docs">>(props: SidebarProps<T>) {
   return (
-    <ul class={styles.fileTree}>
+    <ul class={s.fileTree}>
       <For each={props.tree}>
         {(node) => (
           <li>
             {node.type === "folder" ? (
-              <details open>
-                <summary class={styles.summary}>
-                  <strong>{node.id.split("-").join(" ")}</strong>
+              <details class={s.details} open>
+                <summary class={s.summary}>
+                  {node.id.split("-").join(" ")}
                 </summary>
                 <Sidebar tree={node.items} />
               </details>
             ) : (
-              <a href={`/docs/${node.id}`} class={styles.fileItem}>
+              <a href={`/docs/${node.id}`} class={s.fileItem}>
                 {node.data.title}
               </a>
             )}
