992d2c74f7a5fb0929edfcd6dd491a1388295f33
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-04-01 11:44:16 -0400

    panspread updates

--- packages/audio-worklets/src/worklets/worklet-supersaw.ts
+++ packages/audio-worklets/src/worklets/worklet-supersaw.ts
@@ -11,7 +11,7 @@ const audioParams = [
   { name: "start", defaultValue: 0, max: Number.POSITIVE_INFINITY, min: 0 },
   { name: "stop", defaultValue: 0, max: Number.POSITIVE_INFINITY, min: 0 },
   { name: "frequency", defaultValue: 440, minValue: 20, maxValue: 20000 },
-  { name: "panspread", defaultValue: 0.4, min: 0, max: 1 },
+  { name: "panspread", defaultValue: 0.5, min: 0, max: 1 },
   { name: "freqspread", defaultValue: 0.2, min: 0, max: 1 },
   { name: "detune", defaultValue: 0, minValue: -153600.0, maxValue: 153600.0 },
   { name: "voices", defaultValue: 7, min: 1, max: 8, automationRate: "k-rate" },
@@ -73,10 +73,12 @@ class SuperSawOscillatorProcessor extends AudioWorkletProcessor {
       freq = freq * Math.pow(2, detune / 100 / 12);
       const detuner = getDetuner(voices, freqspread);
 
-      let gainL = Math.sqrt(1 - panspread);
-      let gainR = Math.sqrt(panspread);
-
       for (let v = 0; v < voices; v++) {
+        const pan = voices > 1 ? ((v / (voices - 1)) * 2 - 1) * panspread : 0;
+        const angle = (pan + 1) * (Math.PI / 4);
+        const gainL = Math.cos(angle);
+        const gainR = Math.sin(angle);
+
         const phase = this.phase[v];
         const voiceFrequency = freq * Math.pow(2, detuner(v) / 12);
         const dt = frac(voiceFrequency / sampleRate);
@@ -85,7 +87,6 @@ class SuperSawOscillatorProcessor extends AudioWorkletProcessor {
         outL[i] += sample * gainL;
         outR[i] += sample * gainR;
         this.phase[v] = (phase + dt) % 1;
-        [gainL, gainR] = [gainR, gainL]; // Swap pan for next voice
       }
     }
     return true;
