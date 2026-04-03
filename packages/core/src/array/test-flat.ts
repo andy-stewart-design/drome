12ae7d439853db0e60cddff5449a266a284ee273
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-04-03 07:37:54 -0400

    adding all methods

--- packages/core/src/array/test-flat.ts
+++ packages/core/src/array/test-flat.ts
@@ -1,19 +1,34 @@
+import { euclid } from "@/utils/euclid";
+import { hex } from "@/utils/hex";
+
 type Nullable<T> = T | null | undefined;
+type NoteInput<T> = T | T[];
+type Cycle<T> = T[][];
 
-type NoteInput<T> = T | (T | T[]);
-
+// -----------------------------------------------------------------
 // HELPER FUNCTIONS
 
-function note<T>(...input: NoteInput<T>[]) {
-  const value = input.map((cycle) => (Array.isArray(cycle) ? cycle : [cycle]));
-  return value;
+function pattern<T>(...patterns: NoteInput<T>[]) {
+  return patterns.map((p) => (Array.isArray(p) ? p : [p]));
 }
 
-function stretch<T>(arr: T[][], bars: number, steps = 1) {
+function arrange<T>(...patterns: [number, NoteInput<T>][]) {
+  let nextCycle: Cycle<T> = [];
+
+  for (const [numLoops, pattern] of patterns) {
+    for (let i = 0; i < numLoops; i++) {
+      nextCycle.push(Array.isArray(pattern) ? pattern : [pattern]);
+    }
+  }
+
+  return nextCycle;
+}
+
+function stretch<T>(arr: Cycle<T>, bars: number, steps = 1) {
   bars = Math.round(bars);
   steps = Math.round(steps);
 
-  const nextCycles: T[][] = [];
+  const nextCycles: Cycle<T> = [];
 
   for (const cycle of arr) {
     const expanded =
@@ -26,27 +41,117 @@ function stretch<T>(arr: T[][], bars: number, steps = 1) {
   return nextCycles;
 }
 
-function reverse<T>(arr: T[][]) {
+function reverse<T>(arr: Cycle<T>) {
   return arr
     .slice()
     .reverse()
     .map((arr) => arr?.slice().reverse());
 }
 
+function fast<T>(cycle: Cycle<T>, nullVal: T, mult: number): Cycle<T> | null {
+  mult = Math.round(mult);
+  if (mult === 1) return null;
+  else if (mult < 1) return slow(cycle, nullVal, 1 / mult);
+
+  const length = Math.ceil(cycle.length / mult);
+  const numLoops = mult * length;
+  const nextCyle: Cycle<T> = Array.from({ length }, () => []);
+
+  for (let i = 0; i < numLoops; i++) {
+    const v = cycle[i % cycle.length];
+    nextCyle[Math.floor(i / mult)].push(...v);
+  }
+
+  return nextCyle;
+}
+
+function slow<T>(cycle: Cycle<T>, nullVal: T, mult: number): Cycle<T> | null {
+  mult = Math.round(mult);
+  if (mult === 1) return null;
+  else if (mult < 1) return fast(cycle, nullVal, 1 / mult);
+
+  const nextCycle: Cycle<T> = [];
+
+  for (const pat of cycle) {
+    const expanded: Cycle<T>[number] = [];
+    for (let i = 0; i < pat.length * mult; i++) {
+      expanded.push(i % mult === 0 ? pat[i / mult] : nullVal);
+    }
+    for (let k = 0; k < mult; k++) {
+      nextCycle.push(expanded.slice(k * pat.length, (k + 1) * pat.length));
+    }
+  }
+
+  return nextCycle;
+}
+
+function sequence(stepCount: number, ...steps: (number | number[])[]) {
+  return steps.map((p) =>
+    Array.from({ length: stepCount }, (_, i) => {
+      return [p].flat().includes(i) ? 1 : 0;
+    }),
+  );
+}
+
+function xox(...steps: (number | number[])[] | string[]) {
+  return steps.map((c) => {
+    if (typeof c === "string") {
+      return c.split("").reduce<number[]>((acc, s) => {
+        if (s.trim()) acc.push(s.trim() === "x" ? 1 : 0);
+        return acc;
+      }, []);
+    }
+    return Array.isArray(c) ? c.map((n) => (n ? 1 : 0)) : c ? [1] : [0];
+  });
+}
+
+// -----------------------------------------------------------------
 // MAIN CLASS
 
 class CycleArray<T> {
-  protected _value: T[][];
+  private _value: Cycle<T>;
+  private _nullValue: T;
 
-  constructor(...input: NoteInput<T>[]) {
-    this._value = input.map((c) => (Array.isArray(c) ? c : [c]));
+  constructor(pattern: NoteInput<T>, nullValue: T) {
+    this._value = Array.isArray(pattern) ? [pattern] : [[pattern]];
+    this._nullValue = nullValue;
   }
 
-  note(...input: NoteInput<T>[]) {
-    this._value = note(...input);
+  private applyPattern(modifier: (number | null | undefined)[][]) {
+    const cycles = this._value;
+    const loops = Math.max(cycles.length, modifier.length);
+    const nextCycles: Cycle<T> = [];
+
+    for (let i = 0; i < loops; i++) {
+      let noteIndex = 0;
+      const cycle = cycles[i % cycles.length] ?? [];
+
+      const nextCycle = modifier[i % modifier.length].map((p) =>
+        p === 0 ? this._nullValue : cycle[noteIndex++ % cycle.length],
+      );
+
+      nextCycles.push(nextCycle);
+    }
+
+    return nextCycles;
+  }
+
+  /* ----------------------------------------------------------------
+  /* PATTERN SETTERS
+  ---------------------------------------------------------------- */
+  pattern(...patterns: NoteInput<T>[]) {
+    this._value = pattern(...patterns);
     return this;
   }
 
+  arrange(...input: [number, NoteInput<T>][]) {
+    this._value = arrange(...input);
+    return this;
+  }
+
+  /* ----------------------------------------------------------------
+  /* PATTERN MODIFIERS
+  ---------------------------------------------------------------- */
   stretch(bars: number, steps = 1) {
     this._value = stretch(this._value, bars, steps);
     return this;
@@ -57,7 +162,44 @@ class CycleArray<T> {
     return this;
   }
 
-  at(i: number): T[];
+  fast(mult: number) {
+    const nextCycle = fast(this._value, this._nullValue, mult);
+    if (!nextCycle) return this;
+    this._value = nextCycle;
+    return this;
+  }
+
+  slow(mult: number) {
+    const nextCycle = slow(this._value, this._nullValue, mult);
+    if (!nextCycle) return this;
+    this._value = nextCycle;
+    return this;
+  }
+
+  euclid(pulses: number | number[], steps: number, rot?: number | number[]) {
+    this._value = this.applyPattern(euclid(pulses, steps, rot));
+    return this;
+  }
+
+  hex(...input: (string | number)[]) {
+    this._value = this.applyPattern(input.map(hex));
+    return this;
+  }
+
+  sequence(stepCount: number, ...steps: (number | number[])[]) {
+    this._value = this.applyPattern(sequence(stepCount, ...steps));
+    return this;
+  }
+
+  xox(...steps: (number | number[])[] | string[]) {
+    this._value = this.applyPattern(xox(...steps));
+    return this;
+  }
+
+  /* ----------------------------------------------------------------
+    /* GETTERS
+    ---------------------------------------------------------------- */
+  at(i: number): Cycle<T>[number];
   at(i: number, j: number): T;
   at(i: number, j?: number) {
     const currentValue = this.value[i % this.value.length];
@@ -72,11 +214,14 @@ class CycleArray<T> {
   }
 }
 
-const myCycles = new CycleArray<number>(60);
-myCycles.note([0, 4, 2, 0], 0);
+const myCycles = new CycleArray<number>(60, 0);
+myCycles.pattern([0, 4, 2, 0], 0);
 const cycles = myCycles.value;
 console.log(cycles);
-const pattern = myCycles.at(0);
-console.log(pattern);
+const pat = myCycles.at(0);
+console.log(pat);
 const scheduledValue = myCycles.at(0, 0);
 console.log(scheduledValue);
+
+const myCycles2 = new CycleArray<Nullable<number>>([60, null], 0);
+console.log(myCycles2);
--- packages/core/src/array/test-nested.ts
+++ packages/core/src/array/test-nested.ts
@@ -1,52 +1,160 @@
+// TODO: Move applyPattern and associated methods
+// euclid(): number[][]
+// hex(): number[] **which is then nested**
+import { euclid } from "@/utils/euclid";
+import { hex } from "@/utils/hex";
+
 type Nullable<T> = T | null | undefined;
-
 type NoteInput<T> = T | (T | T[])[];
+type Cycle<T> = (T | T[])[][];
 
+// -----------------------------------------------------------------
 // HELPER FUNCTIONS
 
-function note<T>(...input: NoteInput<T>[]) {
-  const value = input.map((cycle) => (Array.isArray(cycle) ? cycle : [cycle]));
-  return value;
+function pattern<T>(...patterns: NoteInput<T>[]) {
+  return patterns.map((p) => (Array.isArray(p) ? p : [p]));
 }
 
-function stretch<T>(arr: (T | T[])[][], bars: number, steps = 1) {
-  bars = Math.round(bars);
-  steps = Math.round(steps);
+function arrange<T>(...patterns: [number, NoteInput<T>][]) {
+  let nextCycle: Cycle<T> = [];
 
-  const nextCycles: T[][] = [];
-
-  for (const cycle of arr) {
-    const expanded =
-      steps > 1 ? cycle.flatMap((step) => Array(steps).fill(step)) : cycle;
-    for (let k = 0; k < Math.max(bars, 1); k++) {
-      nextCycles.push([...expanded]);
+  for (const [numLoops, pattern] of patterns) {
+    for (let i = 0; i < numLoops; i++) {
+      nextCycle.push(Array.isArray(pattern) ? pattern : [pattern]);
     }
   }
 
-  return nextCycles;
+  return nextCycle;
 }
 
-function reverse<T>(arr: (T | T[])[][]) {
-  return arr
+function stretch<T>(cycle: (T | T[])[][], bars: number, steps = 1) {
+  bars = Math.round(bars);
+  steps = Math.round(steps);
+
+  const nextCycle: T[][] = [];
+
+  for (const pattern of cycle) {
+    const expanded =
+      steps > 1 ? pattern.flatMap((step) => Array(steps).fill(step)) : pattern;
+    for (let k = 0; k < Math.max(bars, 1); k++) {
+      nextCycle.push([...expanded]);
+    }
+  }
+
+  return nextCycle;
+}
+
+function reverse<T>(cycle: Cycle<T>) {
+  return cycle
     .slice()
     .reverse()
     .map((arr) => arr?.slice().reverse());
 }
 
+function fast<T>(cycle: Cycle<T>, nullVal: T, mult: number): Cycle<T> | null {
+  mult = Math.round(mult);
+  if (mult === 1) return null;
+  else if (mult < 1) return slow(cycle, nullVal, 1 / mult);
+
+  const length = Math.ceil(cycle.length / mult);
+  const numLoops = mult * length;
+  const nextCyle: Cycle<T> = Array.from({ length }, () => []);
+
+  for (let i = 0; i < numLoops; i++) {
+    const v = cycle[i % cycle.length];
+    nextCyle[Math.floor(i / mult)].push(...v);
+  }
+
+  return nextCyle;
+}
+
+function slow<T>(cycle: Cycle<T>, nullVal: T, mult: number): Cycle<T> | null {
+  mult = Math.round(mult);
+  if (mult === 1) return null;
+  else if (mult < 1) return fast(cycle, nullVal, 1 / mult);
+
+  const nextCycle: Cycle<T> = [];
+
+  for (const pat of cycle) {
+    const expanded: Cycle<T>[number] = [];
+    for (let i = 0; i < pat.length * mult; i++) {
+      expanded.push(i % mult === 0 ? pat[i / mult] : nullVal);
+    }
+    for (let k = 0; k < mult; k++) {
+      nextCycle.push(expanded.slice(k * pat.length, (k + 1) * pat.length));
+    }
+  }
+
+  return nextCycle;
+}
+
+function sequence(stepCount: number, ...steps: (number | number[])[]) {
+  return steps.map((p) =>
+    Array.from({ length: stepCount }, (_, i) => {
+      return [p].flat().includes(i) ? 1 : 0;
+    }),
+  );
+}
+
+function xox(...steps: (number | number[])[] | string[]) {
+  return steps.map((c) => {
+    if (typeof c === "string") {
+      return c.split("").reduce<number[]>((acc, s) => {
+        if (s.trim()) acc.push(s.trim() === "x" ? 1 : 0);
+        return acc;
+      }, []);
+    }
+    return Array.isArray(c) ? c.map((n) => (n ? 1 : 0)) : c ? [1] : [0];
+  });
+}
+
+// -----------------------------------------------------------------
 // MAIN CLASS
 
 class CycleArrayNested<T> {
-  protected _value: (T | T[])[][];
+  private _value: Cycle<T>;
+  private _nullValue: T;
 
-  constructor(...input: NoteInput<T>[]) {
-    this._value = input.map((c) => (Array.isArray(c) ? c : [c]));
+  constructor(pattern: NoteInput<T>, nullValue: T) {
+    this._value = Array.isArray(pattern) ? [pattern] : [[pattern]];
+    this._nullValue = nullValue;
   }
 
-  note(...input: NoteInput<T>[]) {
-    this._value = note(...input);
+  private applyPattern(modifier: (number | null | undefined)[][]) {
+    const cycles = this._value;
+    const loops = Math.max(cycles.length, modifier.length);
+    const nextCycles: Cycle<T> = [];
+
+    for (let i = 0; i < loops; i++) {
+      let noteIndex = 0;
+      const cycle = cycles[i % cycles.length] ?? [];
+
+      const nextCycle = modifier[i % modifier.length].map((p) =>
+        p === 0 ? this._nullValue : cycle[noteIndex++ % cycle.length],
+      );
+
+      nextCycles.push(nextCycle);
+    }
+
+    return nextCycles;
+  }
+
+  /* ----------------------------------------------------------------
+  /* PATTERN SETTERS
+  ---------------------------------------------------------------- */
+  pattern(...patterns: NoteInput<T>[]) {
+    this._value = pattern(...patterns);
     return this;
   }
 
+  arrange(...input: [number, NoteInput<T>][]) {
+    this._value = arrange(...input);
+    return this;
+  }
+
+  /* ----------------------------------------------------------------
+  /* PATTERN MODIFIERS
+  ---------------------------------------------------------------- */
   stretch(bars: number, steps = 1) {
     this._value = stretch(this._value, bars, steps);
     return this;
@@ -57,7 +165,44 @@ class CycleArrayNested<T> {
     return this;
   }
 
-  at(i: number): (T | T[])[];
+  fast(mult: number) {
+    const nextCycle = fast(this._value, this._nullValue, mult);
+    if (!nextCycle) return this;
+    this._value = nextCycle;
+    return this;
+  }
+
+  slow(mult: number) {
+    const nextCycle = slow(this._value, this._nullValue, mult);
+    if (!nextCycle) return this;
+    this._value = nextCycle;
+    return this;
+  }
+
+  euclid(pulses: number | number[], steps: number, rot?: number | number[]) {
+    this._value = this.applyPattern(euclid(pulses, steps, rot));
+    return this;
+  }
+
+  hex(...input: (string | number)[]) {
+    this._value = this.applyPattern(input.map(hex));
+    return this;
+  }
+
+  sequence(stepCount: number, ...steps: (number | number[])[]) {
+    this._value = this.applyPattern(sequence(stepCount, ...steps));
+    return this;
+  }
+
+  xox(...steps: (number | number[])[] | string[]) {
+    this._value = this.applyPattern(xox(...steps));
+    return this;
+  }
+
+  /* ----------------------------------------------------------------
+  /* GETTERS
+  ---------------------------------------------------------------- */
+  at(i: number): Cycle<T>[number];
   at(i: number, j: number): T | T[];
   at(i: number, j?: number) {
     const currentValue = this.value[i % this.value.length];
@@ -67,11 +212,15 @@ class CycleArrayNested<T> {
     return currentValue;
   }
 
+  get length() {
+    return this._value.length;
+  }
+
   get value() {
     return this._value;
   }
 }
 
-const myCycles = new CycleArrayNested<Nullable<number>>(60);
-myCycles.note([0, 4, [2, 0]], 0);
+const myCycles = new CycleArrayNested<Nullable<number>>(60, null);
+myCycles.pattern([0, 4, [2, 0]], 0);
 console.log(myCycles.value);
