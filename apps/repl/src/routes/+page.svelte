cf9565d05ae0d5aa351c0e5881b7087765e3026f
andy-stewart-design <47165383+andy-stewart-design@users.noreply.github.com>
2026-04-10 11:59:09 -0400

    initial drome setup

--- apps/repl/src/routes/+page.svelte
+++ apps/repl/src/routes/+page.svelte
@@ -1,17 +1,53 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { createCodeMirror } from '@drome/editor';
+	import type Drome from 'drome-live';
 
-	let editorEl: HTMLDivElement;
+	let container: HTMLDivElement;
+	let editor: ReturnType<typeof createCodeMirror> | null = $state(null);
+	let drome: Drome | null = $state(null);
 
 	onMount(() => {
-		createCodeMirror(editorEl, 'console.log("hello from drome editor");\n');
+		async function init() {
+			const { default: Drome } = await import('drome-live');
+			drome = await Drome.init(120);
+		}
+
+		function togglePlaystate(e: KeyboardEvent) {
+			if (!(e.altKey && e.key === 'Enter')) return;
+			if (drome && !drome.paused) {
+				console.log('stopping');
+				drome.stop();
+				// v?.stop();
+			} else if (drome && editor) {
+				console.log('starting');
+				const code = editor.state.doc.toString();
+				drome.evaluate(code);
+				// flash();
+				if (drome.paused) drome.start();
+				// if (v?.paused) v?.start();
+			}
+		}
+
+		editor = createCodeMirror(
+			container,
+			'd.synth("tri").root("a3").scale("min").note([0,2,4,6]).push()'
+		);
+		init();
+
+		window.addEventListener('keydown', togglePlaystate);
+		return () => window.removeEventListener('keydown', togglePlaystate);
+	});
+
+	$effect(() => {
+		console.log(editor?.state.doc.toString());
+		console.log(drome);
 	});
 </script>
 
 <h1 class="sr-only">Drome REPL</h1>
 <div class="grid">
-	<div bind:this={editorEl} class="container"></div>
+	<div bind:this={container} class="container"></div>
 </div>
 
 <style>
